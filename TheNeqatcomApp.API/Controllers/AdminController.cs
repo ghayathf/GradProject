55ef83f59af135ce8d636a9f6b241c65a3d9eef7
BushraKhattabAlawneh <bkalawneh19@cit.just.edu.jo>
2023-05-31 15:45:22 +0300

    vv

---
+++ TheNeqatcomApp.API/Controllers/AdminController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheNeqatcomApp.Core.Data;
+using TheNeqatcomApp.Core.DTO;
+using TheNeqatcomApp.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neqatcom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+
+        private readonly IAdminService adminService;
+        private readonly ILoaneeComplaintsService lcService;
+
+        public AdminController(IAdminService adminService,ILoaneeComplaintsService lcService)
+        {
+            this.adminService = adminService;
+            this.lcService = lcService;
+        }
+        [HttpPut]
+        [Route("deleteComplaint/{cid}")]
+        public void deleteComplaint(int cid)
+        {
+            adminService.deleteComplaint(cid);
+        }
+        [HttpGet]
+        [Route("categoriesStatistics")]
+        public CategoriesStatistics categoriesStatistics()
+        {
+            return adminService.categoriesStatistics();
+        }
+        [HttpGet]
+        [Route("complaintsStatistics")]
+        public ComplaintsStatistics complaintsStatistics()
+        {
+            return adminService.complaintsStatistics();
+        }
+        [HttpGet]
+        [Route("lenderAdminStatistics")]
+        public LenderAdminStatistics lenderAdminStatistics()
+        {
+            return adminService.lenderAdminStatistics();
+        }
+        [HttpGet]
+        [Route("AdminStatisticsLoanee")]
+        public AdminStatisticsLoanee AdminStatisticsLoanee()
+        {
+            return adminService.AdminStatisticsLoanee();
+        }
+        [HttpGet]
+        [Route("GetAllCommercialRegistres")]
+        public List<Gpcommercialregister> GetAllCommecialregister()
+        {
+            return adminService.GetGpcommercialregisters();
+        }
+        [HttpPut]
+        [Route("AcceptLenderRegistration/{id}")]
+        public void AcceptLenderRegistration(int id)
+        {
+            adminService.HandleRegistarction(id);
+        }
+        [HttpPost]
+        [Route("ManageLoaneeComplaints/{lid}/{CID}")]
+        public void ManageLoaneeComplaints(int lid, int CID)
+        {
+            lcService.ManageComplaints(lid, CID);
+        }
+        [HttpGet]
+        [Route("GetAllComplaints")]
+        public List<LoaneeComplaintsDTO> GetAllComplaints()
+        {
+            return lcService.GetAllCompliants();
+        }
+        [HttpPost]
+        [Route("CheckFiveDays")]
+        public void CheckFiveDays()
+        {
+            lcService.CheckFiveDays();
+        }
+        [HttpGet]
+        [Route("LoaneeCreditScores")]
+        public List<LoaneeCreditScores> LoaneeCreditScores()
+        {
+            return adminService.loaneeCreditScores();
+        }
+        [HttpGet]
+        [Route("ActorCounter")]
+        public List<ActorCounterDTO> ActorCounter()
+        {
+            return adminService.ActorCounter();
+        }
+        [HttpGet]
+        [Route("GetLenderStoresComplaints")]
+        public List<LenderComplaints> GetLenderStoresComplaints()
+        {
+            return adminService.GetLenderStoresComplaints();
+        }
+        [HttpPost]
+        [Route("ManageLenderComplaints/{loaid}/{CID}")]
+        public void ManageLenderComplaints(int loaid, int CID)
+        {
+            adminService.ManageLenderComplaints(loaid, CID);
+        }
+        [HttpGet]
+        [Route("CancleLoanAutomatically")]
+        public List<CancleLoanAuto> CancleLoanAutomatically()
+        {
+            return adminService.CancleLoanAutomatically();
+        }
+        [HttpGet]
+        [Route("CancleLoanAutoMsgForLender")]
+        public List<CancleLoanMsgforLender> CancleLoanAutoMsgForLender()
+        {
+            return adminService.CancleLoanAutoMsgForLender();
+        }
+
+    }
+}
--- TheNeqatcomApp.Infra/Repository/AdminRepository.cs
+++ TheNeqatcomApp.Infra/Repository/AdminRepository.cs
@@ -22,37 +22,101 @@ namespace TheNeqatcomApp.Infra.Repository
         public AdminRepository() { }
         public List<ActorCounterDTO> ActorCounter()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT Role, COUNT(*) AS Count
+        FROM GPUser
+        GROUP BY Role
+        ORDER BY Role";
+
+            IEnumerable<ActorCounterDTO> loan = dbContext.Connection.Query<ActorCounterDTO>(sql);
+            return loan.ToList();
         }
 
         public AdminStatisticsLoanee AdminStatisticsLoanee()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT
+            (SELECT COUNT(*) FROM GPLoanee) AS loaneesCount,
+            (SELECT ROUND(AVG(CreditScore)) FROM GPLoanee) AS averageCreditScore,
+            (SELECT COUNT(*) FROM GPLoan) AS totalLoansCount
+        FROM DUAL";
+
+            IEnumerable<AdminStatisticsLoanee> loan = dbContext.Connection.Query<AdminStatisticsLoanee>(sql);
+            return loan.FirstOrDefault();
         }
 
         public List<CancleLoanAuto> CancleLoanAutomatically()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT gploan.*, gploanee.*, gpuser.*, gpmeetings.*
+        FROM gploan
+        INNER JOIN gploanee ON gploan.loaneeid = gploanee.loaneeid
+        INNER JOIN gpuser ON gploanee.LOANEEUSERID = gpuser.userid
+        INNER JOIN gpmeetings ON gpmeetings.loanid = gploan.loanid
+        WHERE TRUNC(gpmeetings.startdate) + 3 <= TRUNC(SYSDATE)
+        AND (gploan.loanstatus = 1 OR gploan.loanstatus = 2)";
+
+            IEnumerable<CancleLoanAuto> loan = dbContext.Connection.Query<CancleLoanAuto>(sql);
+            return loan.ToList();
         }
 
         public List<CancleLoanMsgforLender> CancleLoanAutoMsgForLender()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT gploan.*, gpmeetings.*, gplenderstore.*, gpuser.*
+        FROM gploan
+        INNER JOIN gpmeetings ON gploan.loanid = gpmeetings.loanid
+        INNER JOIN gplenderstore ON gplenderstore.Lenderid = gpmeetings.lenderid
+        INNER JOIN gpuser ON gplenderstore.lenderuserid = gpuser.userid
+        WHERE TRUNC(gpmeetings.startdate) + 3 <= TRUNC(SYSDATE)
+        AND (gploan.loanstatus = 1 OR gploan.loanstatus = 2)";
+
+            IEnumerable<CancleLoanMsgforLender> loan = dbContext.Connection.Query<CancleLoanMsgforLender>(sql);
+            return loan.ToList();
         }
 
         public CategoriesStatistics categoriesStatistics()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT (
+            SELECT COUNT(*) FROM GPCategory
+        ) AS TotalCategories,
+        (
+            SELECT COUNT(*) FROM GPOffer
+        ) AS TotalOffers,
+        (
+            SELECT COUNT(*) FROM GPLoan
+        ) AS TotalLoans
+        FROM DUAL";
+
+            IEnumerable<CategoriesStatistics> loan = dbContext.Connection.Query<CategoriesStatistics>(sql);
+            return loan.FirstOrDefault();
         }
 
         public ComplaintsStatistics complaintsStatistics()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT (
+            SELECT COUNT(*) FROM GPCOMPLAINTS WHERE manageStatus = 1
+        ) AS LoaneeToLenderCount,
+        (
+            SELECT COUNT(*) FROM GPCOMPLAINTS WHERE manageStatus = 2
+        ) AS LenderToLoaneeCount,
+        (
+            SELECT COUNT(*) FROM GPCOMPLAINTS WHERE manageStatus = 3
+        ) AS SystemToLoaneeCount
+        FROM DUAL";
+
+            IEnumerable<ComplaintsStatistics> loan = dbContext.Connection.Query<ComplaintsStatistics>(sql);
+            return loan.FirstOrDefault();
         }
 
         public void deleteComplaint(int cid)
         {
-            throw new NotImplementedException();
+            var sql = "UPDATE GPComplaints SET managestatus = -1 WHERE complaintsid = :CID";
+            var p = new { CID = cid };
+
+            dbContext.Connection.Execute(sql, p);
         }
 
         public List<Gpcommercialregister> GetGpcommercialregisters()
@@ -69,7 +133,15 @@ namespace TheNeqatcomApp.Infra.Repository
 
         public List<LenderComplaints> GetLenderStoresComplaints()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT GPComplaints.*, GPLenderStore.lenderID, GPloanee.*, GPUser.*
+        FROM GPComplaints 
+        LEFT JOIN GPLoanee ON GPComplaints.LOID = GPLoanee.loaneeID
+        LEFT JOIN GPLenderStore ON GPComplaints.LEID = GPLenderStore.lenderID
+        LEFT JOIN GPUser ON GPLoanee.LoaneeuserID = GPUser.userID
+        WHERE GPComplaints.managestatus = 2 OR GPComplaints.managestatus = 3";
+
+            return dbContext.Connection.Query<LenderComplaints>(sql).ToList();
         }
 
         public void HandleRegistarction(int IDD)
@@ -104,17 +176,44 @@ namespace TheNeqatcomApp.Infra.Repository
 
         public LenderAdminStatistics lenderAdminStatistics()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT
+            (SELECT COUNT(*) FROM GPLenderStore WHERE registerStatus = 1) AS registeredLendersCount,
+            (SELECT COUNT(*) FROM GPLenderStore WHERE registerStatus = -1) AS unregisteredLendersCount,
+            COUNT(*) AS totalLendersCount
+        FROM GPLenderStore";
+
+            return dbContext.Connection.QueryFirstOrDefault<LenderAdminStatistics>(sql);
+
         }
 
         public List<LoaneeCreditScores> loaneeCreditScores()
         {
-            throw new NotImplementedException();
+            var sql = @"
+        SELECT CreditScore, COUNT(*) AS Count
+        FROM GPLoanee
+        WHERE CreditScore BETWEEN 1 AND 10
+        GROUP BY CreditScore
+        ORDER BY CreditScore";
+
+            return dbContext.Connection.Query<LoaneeCreditScores>(sql).ToList();
+
         }
 
         public void ManageLenderComplaints(int loaid, int CID)
         {
-            throw new NotImplementedException();
+            var sql = @"
+        UPDATE GPLoanee
+        SET warncounter = NVL(warncounter, 0) + 1
+        WHERE loaneeID = :LoaID;
+
+        UPDATE GPComplaints
+        SET managestatus = 0
+        WHERE complaintsid = :CID";
+
+            var parameters = new { LoaID = loaid, CID = CID };
+
+            dbContext.Connection.Execute(sql, parameters);
         }
     }
 }
